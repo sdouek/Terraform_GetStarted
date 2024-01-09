ef59d05c12950e5ee1b416af6571b2d6709c4160
Sara Douek <64900910+sdouek@users.noreply.github.com>
2024-01-09 10:44:31 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+# main.tf
+
+# AWS Provider
+provider "aws" {
+  region = var.aws_region
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.bucket_name
+  acl    = "private"
+}
+
+# Lambda Function
+resource "aws_lambda_function" "my_lambda_function" {
+  function_name = var.lambda_name
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  filename      = "lambda_function.zip"
+  source_code_hash = filebase64("lambda_function.zip")
+}
+
+# Lambda Permission to access S3
+resource "aws_lambda_permission" "lambda_s3_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda_function.arn
+  principal     = "s3.amazonaws.com"
+}
