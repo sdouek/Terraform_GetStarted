6d9dd994ef4fabc931631027b5de8ac44481143d
Sara Douek <64900910+sdouek@users.noreply.github.com>
2024-01-09 11:17:33 +0200

    Update and rename main.tf.txt to main.tf

---
+++ modules/api_gateway/main.tf
@@ -0,0 +1,34 @@
+# modules/api_gateway/main.tf
+
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name        = var.api_name
+  description = "API Gateway for Lambda function"
+}
+
+resource "aws_api_gateway_resource" "api_gateway_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "resource"
+}
+
+resource "aws_api_gateway_method" "api_gateway_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.api_gateway_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "api_gateway_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id             = aws_api_gateway_resource.api_gateway_resource.id
+  http_method             = aws_api_gateway_method.api_gateway_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda_function.invoke_arn
+}
+
+resource "aws_api_gateway_deployment" "api_gateway_deployment" {
+  depends_on  = [aws_api_gateway_integration.api_gateway_integration]
+  rest_api_id  = aws_api_gateway_rest_api.api_gateway.id
+  stage_name   = "prod"
+}
